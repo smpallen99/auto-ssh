86646c23ee8bfc68b2ad3209ed0b3d97b956b44c
Stephen Pallen <smpallen99@yahoo.com>
2012-05-16 21:51:20 -0400

    remove existing directory

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-sudo git clone ~/myprojects/ucx/tools/auto-ssh /usr/local/auto-ssh && /
+if [ -d /usr/local/auto-ssh ]; then
+  sudo rm -rf /usr/local/auto-ssh
+fi
+sudo git clone ~/myprojects/auto-ssh /usr/local/auto-ssh && \
 cd /usr/local/auto-ssh && sudo make install
