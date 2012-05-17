87236acd3c60d439716686145c99af12c415bfe1
Stephen Pallen <smpallen99@yahoo.com>
2012-05-16 22:58:51 -0400

    fixed local repo issue

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,5 @@
 if [ -d /usr/local/auto-ssh ]; then
   sudo rm -rf /usr/local/auto-ssh
 fi
-sudo git clone ~/myprojects/auto-ssh /usr/local/auto-ssh && \
+sudo git clone https://github.com/smpallen99/auto-ssh /usr/local/auto-ssh && \
 cd /usr/local/auto-ssh && sudo make install
